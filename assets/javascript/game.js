336132d9f0de275d3ed35799561927d8384fac1f
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2019-06-06 15:13:26 -0400

    add functions for score and win/loss <functions not working>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@ var loss = 0;
 
 //Create arrays for random numbers
 
-var arr = Array.apply(null, {length: N}).map(Number.call, Number);
+//var arr = Array.apply(null, {length: N}).map(Number.call, Number);
 
 var computerGuess = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120];
 
@@ -36,5 +36,21 @@ console.log(crystalFour)
 //picks a random number from computerGuess
 //random numbers are assigned to crystals one,two,three,four
 //to keep score we need to add crystals one,two,three,four
+
+function crystalAdd() {
+    [crystalOne] + [crystalTwo] + [crystalThree] + [crystalFour]
+}
+
+crystalAdd()
+console.log(crystalAdd)
+
 //win condition: computerGuess === sum of crystals one,two,three,four
+
+if (computerGuess === crystalAdd()) {
+    win++;
+}
 //loss condition: sum of crystals one,two,three,four > computerGuess
+
+else if (crystalAdd() > computerGuess) {
+    loss++
+}
