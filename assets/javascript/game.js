63e5009f797a9b3863d57d6926f33ff67db4259b
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2019-06-06 00:46:46 -0400

    template array function added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,8 @@ var loss = 0;
 
 //Create arrays for random numbers
 
+var arr = Array.apply(null, {length: N}).map(Number.call, Number);
+
 var computerGuess = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120];
 
 console.log(computerGuess)
@@ -19,7 +21,7 @@ var rand = computerGuess[Math.floor(Math.random() * computerGuess.length)];
 
 console.log(rand)
 
-var crystalOne = crystalNumber[Math.floor(Math.random() 8* crystalNumber.length)];
+var crystalOne = crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
 
 var crystalTwo = crystalNumber[Math.floor(Math.random() * crystalNumber.length)];
 
@@ -30,3 +32,9 @@ console.log(crystalOne)
 console.log(crystalTwo)
 console.log(crystalThree)
 console.log(crystalFour)
+
+//picks a random number from computerGuess
+//random numbers are assigned to crystals one,two,three,four
+//to keep score we need to add crystals one,two,three,four
+//win condition: computerGuess === sum of crystals one,two,three,four
+//loss condition: sum of crystals one,two,three,four > computerGuess
