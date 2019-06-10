5d2c689dd17c34a132908b6022bdaf56e4305404
WildestDeuce <48850844+WildestDeuce@users.noreply.github.com>
2019-06-09 20:59:39 -0400

    fixed jQuery calls for balloons 2-4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,8 +8,6 @@ var score = 0;
 var computerGuess = Array.apply(null, { length: 121 }).map(Number.call, Number);
 computerGuess.splice(0, 19); // removes 0-18 from array
 
-//var computerGuess = [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120];
-
 console.log(computerGuess)
 
 var crystalNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
@@ -48,45 +46,46 @@ $(document).ready(function () {
 
         console.log(score);
     })
+    // function for #number2
     $("#number2").attr("data-value", crystalTwo);
 
     $("#number2").on("click", function () {
         crystalTwo = $(this).attr("data-value");
-        score += parseInt(crystalTwo)
+        score += parseInt(crystalTwo);
 
+        console.log(score);
+    })
+    // function for #number3
 
-        $("#number3").attr("data-value", crystalThree);
+    $("#number3").attr("data-value", crystalThree);
 
-        $("#number3").on("click", function () {
-            crystalThree = $(this).attr("data-value");
-            score += parseInt(crystalThree)
+    $("#number3").on("click", function () {
+        crystalThree = $(this).attr("data-value");
+        score += parseInt(crystalThree);
 
+        console.log(score);
+    })
+    //function for #number4
 
-            $("#number4").attr("data-value", crystalFour);
+    $("#number4").attr("data-value", crystalFour);
 
-            $("#number4").on("click", function () {
-                crystalFour = $(this).attr("data-value");
-                score += parseInt(crystalFour)
-                // function for #number2
+    $("#number4").on("click", function () {
+        crystalFour = $(this).attr("data-value");
+        score += parseInt(crystalFour)
+        console.log(score);
+    })
 
-                // function for #number3
-            })
+    //win condition: computerGuess === sum of crystals one,two,three,four
 
-            function crystalAdd() {
-                //crystalOne + crystalTwo + crystalThree + crystalFour === score
-                // score === $.add(crystalOne,crystalTwo,crystalThree,crystalFour)
-            }
+    if (computerGuess === score) {
+        win++;
+        $("#win").html("<h3>" + win + "</h3>");
+    }
 
-            crystalAdd()
-            //console.log(crystalAdd())
+    //loss condition: sum of crystals one,two,three,four > computerGuess
 
-            //win condition: computerGuess === sum of crystals one,two,three,four
-
-            if (computerGuess === crystalAdd()) {
-                win++;
-            }
-            //loss condition: sum of crystals one,two,three,four > computerGuess
-
-            else if (crystalAdd() > computerGuess) {
-                loss++;
-            }
+    else (computerGuess > score) {
+        lose++;
+        $("#loss").html("<h3>" + loss + "</h3>");
+    }
+});
